a4e86c05af03e559a42e5bf9b86973ec385e051e
Seung Wan Yoo <74849806+wannieman98@users.noreply.github.com>
2021-09-26 14:23:33 -0500

    a

--- temp.py
+++ temp.py
@@ -1,1 +1,11 @@
-print("I am a conflict")
+import random
+import sys
+
+def get_random_order(names):
+    random.shuffle(names)
+
+    print(' '.join(names))
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    get_random_order(args)
