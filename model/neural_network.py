9beb03486dc3d7eb1b3a1444cd1c76da50224788
Seung Wan Yoo <74849806+wannieman98@users.noreply.github.com>
2021-10-11 12:16:18 -0500

    added voc and pcam data processing functions

--- model/neural_network.py
+++ model/neural_network.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 from netowrk_generator import Rand_Wire
 
+
 class RandomlyWiredNeuralNetwork(nn.Module):
     def __init__(self, channel, input_channel, graph_type, classes, node_num, is_small_regime=True):
         self.channel = channel
@@ -24,7 +25,6 @@ class RandomlyWiredNeuralNetwork(nn.Module):
             self.conv4 = Rand_Wire(graph_type, node_num, 2*channel)
             
             self.conv5 = Rand_Wire(graph_type, node_num, 4*channel)
-
 
             self.class_conv = nn.Sequential(
                 nn.ReLU(),
@@ -52,11 +52,12 @@ class RandomlyWiredNeuralNetwork(nn.Module):
             nn.LogSoftmax()
         )
 
+    # noinspection PyCallingNonCallable
     def forward(self, x):
                           #                     x = [B, classes, 96, 96]       
                           #             Small Regime        |         Regular Regime 
         x = self.conv1(x) #                      [B, channel/2, 48, 48]
-        x = self.conv2(x) #         [B, channel, 24, 24]       [B, channel, 24, 24]
+        x = self.conv2  #         [B, channel, 24, 24]       [B, channel, 24, 24]
         x = self.conv3(x) #         [B, channel, 12, 12]       [B, 2*channel, 12, 12]
         x = self.conv4(x) #         [B, 2*channel, 6, 6]       [B, 4*channel, 6, 6]
         x = self.conv5(x) #         [B, 4*channel, 3, 3]       [B, 8*channel, 3, 3]
