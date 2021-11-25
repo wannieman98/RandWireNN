d5d539e44b9a81f251d80987e2b5c61f9edb1b39
Seung Wan Yoo <74849806+wannieman98@users.noreply.github.com>
2021-11-25 06:18:27 -0500

    node operation and random generator

--- model/node.py
+++ model/node.py
@@ -2,7 +2,20 @@ import torch
 import torch.nn as nn
 
 class Node(nn.Module):
-    def __init__(self, in_channel, out_channel, degree, stride=1):
+
+    def __init__(self, in_channel, out_channel, degree=1, stride=1):
+        """Node Operation defined by the paper.
+
+           Node Operation:
+            1. Aggregation of the input nodes.
+            2. Transformation through the convolutional layers.
+
+           Args:
+              aggregate_weight(tensor): Weight to aggregate the node tensors.
+              conv(Module): Convolutional layers defined by the paper.
+              degree(int): The in_degree of the node.
+              stride(int): The stride of the seperable conv layer.
+        """
         super(Node, self).__init__()
         self.aggregate_weight = torch.randn(degree, requires_grad=True)
         self.conv = nn.Sequential(
@@ -10,30 +23,28 @@ class Node(nn.Module):
             SeperableConvolution(in_channel, out_channel, stride),
             nn.BatchNorm2d(out_channel)
         )
-        if isinstance(degree, int):
-            self.degree = [degree]
-        else:
-            self.degree = degree
+        self.degree = degree
 
     def forward(self, x):
         # x = [B, channel, W, H, degree]
         # aggregate weight = [degree, 1]
-        if len(self.degree) >= 1 and len(x.shape) > 4:
+        if self.degree >= 1 and len(x.shape) > 4:
             x = torch.matmul(x, torch.sigmoid(self.aggregate_weight))
+            
         # x = [B, channel, W, H]
         x = self.conv(x)
 
         return x
 
-
 class SeperableConvolution(nn.Module):
+
     def __init__(self, in_channel, out_channel, stride=1):
         super(SeperableConvolution, self).__init__()
         """
-        Depthwise Convolution: Type of convolution where we apply a single convolutional 
-        filter for each input channels and keeps the convolutions of each channels seperate
-        Convolution: Type of convolution that apply a convolutional filter of over the input 
-        channels as a whole and mix the elements among the channels.
+        Seperable convolution defined by the paper.
+
+        Combination of depthwise 3x3 convolution and 1x1 convolution would act 
+        as convolution for each channels and then taking a channel-wise pooling.
         """
         self.conv = nn.Sequential(
             nn.Conv2d(in_channel, in_channel, 3, groups=in_channel, padding=1, stride=stride),
@@ -41,9 +52,5 @@ class SeperableConvolution(nn.Module):
         )
 
     def forward(self, x):
-        """
-        The combination of depthwise convolution and 1x1 convolution would act 
-        as convolution for each channels, and then taking a channel-wise pooling.
-        """
         # x = [B, C, W, H]
         return self.conv(x)
