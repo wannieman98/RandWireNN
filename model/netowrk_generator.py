0d977af0654eb4e80e99cacf1655f6e5d5eecc8d
Seung Wan Yoo <74849806+wannieman98@users.noreply.github.com>
2021-11-25 08:31:07 -0500

    updated generator

--- model/netowrk_generator.py
+++ model/netowrk_generator.py
@@ -1,56 +1,47 @@
 import torch
+import pickle
 from node import Node
 import torch.nn as nn
 from graph import Graph
 
 
 class Rand_Wire(nn.Module):
-    def __init__(self, node_num, p, in_channels, out_channels, graph_mode, is_train, name):
+
+    def __init__(self, node_num, p, k, m, graph_mode, in_channels, out_channels, is_train, name):
+        """Generator mapping the random graph onto the CNN architecture.
+
+           Args:
+              input_node(int): Node connected to all original input nodes.
+              output_node(int): Node connected to all original output nodes.
+              graph_mode(str): Type of random graph the object will generate.
+              first_node(Module): node operation for all nodes after input node.
+              is_train(bool): whether the model is in training phase.
+              name(str): a unique name to identify graphs.
+              graph(dict): post-processed random graph.
+        """
         super(Rand_Wire, self).__init__()
-        self.node_num = node_num
-        self.p = p
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.graph_mode = graph_mode
-        self.is_train = is_train
-        self.name = name
+        self.input_node = 0
+        self.output_node = node_num + 1
+        self.first_node = Node(in_channels, out_channels, 2)
 
-        # get graph nodes and in edges
-        graph_node = Graph(self.node_num, self.p, graph_mode=graph_mode)
+        if is_train:
+            self.graph = Graph(node_num, p, k, m, graph_mode)
+            self.save_graph()
+        else:
+            self.graph = self.load_graph()
 
-        # if self.is_train is True:
-        #     print("is_train: True")
-        #     graph = graph_node.make_graph()
-        #     self.nodes, self.in_edges = graph_node.get_graph_info(graph)
-        #     graph_node.save_random_graph(graph, name)
-        # else:
-        #     graph = graph_node.load_random_graph(name)
-        #     self.nodes, self.in_edges = graph_node.get_graph_info(graph)
+    def save_graph(self):
+        pass
 
-        # # define input Node
-        # self.module_list = nn.ModuleList([Node(self.in_channels, self.out_channels, self.in_edges[0], stride=2)])
-        # # define the rest Node
-        # self.module_list.extend([Node(self.in_channels, self.out_channels, self.in_edges[node]) for node in self.nodes if node > 0])
+    def load_graph(self):
+        pass
 
     def forward(self, x):
-        memory = {}
-        # start vertex
-        out = self.module_list[0].forward(x)
-        memory[0] = out
+        node_data = {}
+        input_nodes = self.graph[self.input_node]
 
-        # the rest vertex
-        for node in range(1, len(self.nodes) - 1):
-            if len(self.in_edges[node]) > 1:
-                out = self.module_list[node].\
-                    forward(*[memory[in_vertex]
-                              for in_vertex in self.in_edges[node]])
-            else:
-                out = self.module_list[node].\
-                    forward(memory[self.in_edges[node][0]])
-            memory[node] = out
-
-        out = memory[self.in_edges[self.node_num + 1][0]]
-        for in_vertex_index in range(1, len(self.in_edges[self.node_num + 1])):
-            out += memory[self.in_edges[self.node_num + 1][in_vertex_index]]
-        out = out / len(self.in_edges[self.node_num + 1])
-        return out
+        for in_node, out_node in self.graph.items():
+            if in_node == 0:
+                x = self.first_node(x)
+                for node in out_node:
+                    pass
