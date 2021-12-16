a328a18e1340c50b42f02b363e49cff0a9863fa0
Seung Wan Yoo <74849806+wannieman98@users.noreply.github.com>
2021-12-17 00:55:34 +0900

    edit in training script

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     parser.add_argument('--p', type=float, default=0.75)
     parser.add_argument('--k', type=float, default=4)
     parser.add_argument('--m', type=float, default=5)
-    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--lr', type=float, default=0.005)
     parser.add_argument('--path', type=str, default='/content/')
     parser.add_argument('--dataset', type=str, default='voc')
     parser.add_argument('--channel', type=int, default=78)
